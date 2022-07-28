47f09cdebfbd73dbd60b7c71211cfd169a423b8e
Noman | نعمان | नोमान <kyleslayer@protonmail.com>
2022-07-28 18:11:00 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+const URL = 'mongodb+srv://abc:test@cluster0.abc.mongodb.net/ecommerce?retryWrites=true&w=majority'
+
+const connection = mongoose.connect(URL, {
+  useNewUrlParser: true
+}).then(_ => console.log('Aplicación conectada a MongoDB Atlas'))
+
+export default connection
